08e44144490c476144207fc37e6377b1d57eb557
biswambarpradhan <biswambarp43@gmail.com>
2025-01-29 19:01:34 +0530

    test

--- src/main/java/org/biswa/test.js
+++ src/main/java/org/biswa/test.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import FileTree from "@nosferatu500/react-file-tree";
-import "@nosferatu500/react-file-tree/dist/style.css";
+import SortableTree from "react-sortable-tree";
+import "react-sortable-tree/style.css";
 
 const FileSystemViewer = () => {
   const [fileData, setFileData] = useState([]);
@@ -18,9 +18,7 @@ const FileSystemViewer = () => {
       .then((data) => {
         // Ensure only two levels
         const formattedData = data.map((item) => ({
-          id: item.id,
-          name: item.name,
-          isFolder: item.type === "folder",
+          title: item.name,
           children: item.children || [],
         }));
         setFileData(formattedData);
@@ -38,7 +36,12 @@ const FileSystemViewer = () => {
   return (
     <div>
       <h2>File System Viewer</h2>
-      <FileTree files={fileData} />
+      <div style={{ height: 400 }}>
+        <SortableTree
+          treeData={fileData}
+          onChange={setFileData}
+        />
+      </div>
     </div>
   );
 };
